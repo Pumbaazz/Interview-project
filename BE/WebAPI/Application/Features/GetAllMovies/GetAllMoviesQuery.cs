cd84259f5e4a388723e5f5ed30c5eaa06e758493
Phung Nguyen <phung.nguyen@logixtek.com>
2023-05-24 11:08:41 +0700

    remove all source code, keep tailwindcss and clean code summary only

--- BE/WebAPI/Application/Features/GetAllMovies/GetAllMoviesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using WebAPI.Domain.Model;
-
-namespace WebAPI.Application.Features.GetAllMovies
-{
-    public class GetAllMoviesQuery : IRequest<IEnumerable<Movies>>
-    {
-    }
-}
